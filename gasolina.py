ae0e97fbbb98f30c926fed498c864773abf55d82
BrunoArroyo <brunaoarroyo@gmail.com>
2024-01-10 23:50:46 +0000

    Arquivos gasolina.py, gasolina.png e gasolina.csv foram adicionados

---
+++ gasolina.py
@@ -0,0 +1,15 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Definindo DataFrame
+gasolina_df = pd.read_csv('gasolina.csv', sep=',')
+
+# Plotando um gráfico de barras usando Matplotlib
+gasolina_df.plot(kind='bar', x='dia', y='venda', legend=False)
+plt.title('Preço da gasolina a cada dia')
+plt.xlabel('Dia')
+plt.ylabel('Preço')
+
+# Salvando gráfico como png
+plt.savefig('meu_grafico.png')
